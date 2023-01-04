ebd163b84ec94edd221047f6befaaca23e1fad37
Orionleo <jaiaggarwal60@gmail.com>
2023-01-04 15:24:25 +0530

    q4 and q5

---
+++ Company 1 - Microsoft/q4.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// You are given an integer array nums of length n.
+
+// Assume arrk to be an array obtained by rotating nums by k positions clock-wise. We define the rotation function F on nums as follow:
+
+// F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1].
+// Return the maximum value of F(0), F(1), ..., F(n-1).
+
+
+int maxRotateFunction(vector<int> &nums)
+{
+    long long sum = 0, fn = 0;
+
+    long long len = nums.size();
+
+    for (int i = 0; i < len; i++)
+    {
+        sum += nums[i];
+        fn += (i * nums[i]);
+    }
+
+    long long l = 1, r;
+    long long newfn = fn;
+
+    while(l<len){
+        r = l+len-1;
+        long removed = (l-1)*nums[l-1];
+        long added = r*nums[r%len];
+
+        newfn = newfn - removed + added - sum;
+
+        fn = max(fn,newfn);
+        l++;
+    }
+
+    return (int)fn;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
---
+++ Company 1 - Microsoft/q5.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Given a set of distinct positive integers nums, return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
+
+// answer[i] % answer[j] == 0, or
+// answer[j] % answer[i] == 0
+// If there are multiple solutions, return any of them.
+
+vector<int> largestDivisibleSubset(vector<int> &nums)
+{
+    int n = nums.size();
+    if(n==0) return nums;
+    vector<int> ans;
+    
+    //sort the nums arr to compare only bigger elements to smaller elements
+    sort(nums.begin(),nums.end());
+    
+    //to store the elements to insert in the subset array
+    int maxx = 1;
+    
+    //dp arr to store the maximum size of the subset
+    vector<int> dp(n+1,1);
+
+    
+    for(int i = 1;i<n;i++){
+        for(int j = 0;j<i;j++){
+            //dp arr -     1 1 1 1
+            // given arr - 1 2 4 8
+            //now let's compare 2 and 1, 2 is divisble by 1 and 1+dp[0]>dp[1], thus dp[1] = 1+dp[0]
+            //dp[1] = 2, which is bigger than maxx, thus replacing the max value
+            if(nums[i]%nums[j]==0&&1+dp[j]>dp[i]){
+                dp[i]=1+dp[j];
+                if(maxx<dp[i]){
+                    maxx = dp[i];
+                }
+            }
+        }
+    }
+
+    int prev = -1;//prev is the element which will be compared against as we go from right to left in the array
+    for(int i = n-1;i>=0;i--){
+        //if the element is -1 or divides the prev value, and the number of elements left to be inserted in the sub array is equal to the dp value, it will be inserted
+        if(dp[i]==maxx&&(prev%nums[i]==0||prev==-1)){
+            ans.push_back(nums[i]);
+            maxx-=1;
+            prev = nums[i];
+        }
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
